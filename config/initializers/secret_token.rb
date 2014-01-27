116680c71d553430dc7740c6411d807d18bc786b
dwatling <daniel.watling@gmail.com>
2014-01-27 17:38:50 -0500

    First commit. Bare bones rails structure.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeatherDashboard::Application.config.secret_key_base = '30e35e08e7de309ff3bc412947dfe1a6b74e6ac07776bf62a87c22b8f94d23c14b59de0ec842fcb69711883aedb28a0dffc7a2e846ba0f7ad63c5ad406a26ddf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WeatherDashboard::Application.config.session_store :cookie_store, key: '_weather-dashboard_session'
